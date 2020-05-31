24efb9d0789bf5bbb4097cecbc02875e5d152eee
lihaijun <754491577@qq.com>
2020-05-31 17:43:56 +0800

    连线处理#对象传递

--- workflow/db/domain.go
+++ workflow/db/domain.go
@@ -1,6 +1,8 @@
 package db
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+)
 
 var (
 	flowMap = make(map[string]flow, 0)
@@ -39,95 +41,106 @@ type Process struct {
 	Flow             []flow
 }
 
-type FlowElement struct {
+type BaseElement struct {
 	Id   string `xml:"id,attr"`
 	Name string `xml:"name,attr"`
 }
 
 type Flow struct {
-	FlowElement
+	BaseElement
 	Id           string `xml:"id,attr"`
 	Name         string `xml:"name,attr"`
-	IncomingFlow *[]flow
-	OutgoingFlow *[]flow
+	IncomingFlow []*flow
+	OutgoingFlow []*flow
 }
 
 type StartEvent struct {
-	Flow
+	*Flow
 	StartEventName xml.Name `xml:"startEvent"`
 	Initiator      string   `xml:"initiator,attr"`
 	FormKey        string   `xml:"formKey,attr"`
 }
 
 type EndEvent struct {
-	Flow
+	*Flow
 	EndEventName xml.Name `xml:"endEvent"`
 }
 
 type UserTask struct {
-	Flow
+	*Flow
 	UserTaskName   xml.Name `xml:"userTask"`
 	Assignee       string   `xml:"assignee,attr"`
 	CandidateUsers string   `xml:"candidateUsers,attr"`
 }
 type SequenceFlow struct {
-	Flow
-	SequenceFlowName xml.Name `xml:"sequenceFlow"`
-	Id               string   `xml:"id,attr"`
-	SourceRef        string   `xml:"sourceRef,attr"`
-	TargetRef        string   `xml:"targetRef,attr"`
+	*Flow
+	SequenceFlowName  xml.Name `xml:"sequenceFlow"`
+	Id                string   `xml:"id,attr"`
+	SourceRef         string   `xml:"sourceRef,attr"`
+	TargetRef         string   `xml:"targetRef,attr"`
+	SourceFlowElement *flow
+	TargetFlowElement *flow
 }
 type ExclusiveGateway struct {
-	Flow
+	*Flow
 }
 
 type flow interface {
-	setIncoming(f []flow)
+	setIncoming(f []*flow)
+	setOutgoing(f []*flow)
+	getIncoming() []*flow
+	getOutgoing() []*flow
 }
 
-func (flow *Flow) setIncoming(f []flow) {
-	flow.IncomingFlow = &f
+func (flow *Flow) setIncoming(f []*flow) {
+	flow.IncomingFlow = f
 }
-func (flow *Flow) setOutgoing(f []flow) {
-	flow.OutgoingFlow = &f
+func (flow *Flow) setOutgoing(f []*flow) {
+	flow.OutgoingFlow = f
 }
 
-func setFow() {
-	task := UserTask{}
-	sequence := SequenceFlow{}
-	m := []flow{}
-	m[0] = &sequence
-	task.setIncoming(m)
+func (flow *Flow) getIncoming() []*flow {
+	return flow.IncomingFlow
+}
+func (flow *Flow) getOutgoing() []*flow {
+	return flow.OutgoingFlow
 }
 
 func Converter(d *Definitions) {
 	processes := d.Process
 	if processes != nil {
-		for _, p := range processes {
+		for j, p := range processes {
 			start := p.StartEvent
 			if start != nil {
-				for _, sta := range start {
-					flowMap[sta.Id] = &sta
+				for i, sta := range start {
+					flowMap[sta.Id] = start[i]
 				}
 			}
 			se := p.SequenceFlow
 			if se != nil {
-				for _, s := range se {
-					flowMap[s.Id] = &s
+				for i, s := range se {
+					flowMap[s.Id] = se[i]
 				}
 			}
 			user := p.UserTask
 			if user != nil {
-				for _, u := range user {
-					flowMap[u.Id] = &u
+				for i, u := range user {
+					flowMap[u.Id] = user[i]
 				}
 			}
 			end := p.EndEvent
 			if end != nil {
-				for _, e := range end {
-					flowMap[e.Id] = &e
+				for i, e := range end {
+					flowMap[e.Id] = end[i]
 				}
 			}
+			flows := make([]flow, len(flowMap))
+			count := 0
+			for _, v := range flowMap {
+				flows[count] = v
+				count++
+			}
+			processes[j].Flow = flows
 		}
 	}
 }
@@ -136,40 +149,42 @@ func ConvertXMLToElement(model *Definitions) {
 	processes := model.Process
 	if processes != nil {
 		for _, p := range processes {
-			var start = p.StartEvent
-			if start != nil {
-				for i, sta := range start {
-					value := flowMap[sta.Id]
-					m := make([]flow, 1)
-					m[0] = value
-					start[i].setIncoming(m)
-				}
-			}
-			se := p.SequenceFlow
-			if se != nil {
-				for i, s := range se {
-					value := flowMap[s.Id]
-					m := make([]flow, 1)
-					m[0] = value
-					se[i].setIncoming(m)
-				}
-			}
-			user := p.UserTask
-			if user != nil {
-				for i, u := range user {
-					value := flowMap[u.Id]
-					m := make([]flow, 1)
-					m[0] = value
-					user[i].setIncoming(m)
-				}
-			}
-			end := p.EndEvent
-			if end != nil {
-				for i, e := range end {
-					value := flowMap[e.Id]
-					m := make([]flow, 1)
-					m[0] = value
-					end[i].setIncoming(m)
+			flows := p.Flow
+			for i := range flows {
+				value, ok := flows[i].(SequenceFlow)
+				if ok {
+					SourceRef := value.SourceRef
+					//上一个节点
+					lastFlow := flowMap[SourceRef]
+					if lastFlow != nil {
+						var outgoing = lastFlow.getOutgoing()
+						if outgoing == nil {
+							outgoing = make([]*flow, 0)
+						}
+						newOut := append(outgoing, &flows[i])
+						//设置上一个节点出口
+						lastFlow.setOutgoing(newOut)
+						//设置当前连线入口
+						value.SourceFlowElement = &lastFlow
+
+					}
+					//下一个节点
+					TargetRef := value.TargetRef
+					nextFlow := flowMap[TargetRef]
+					if nextFlow != nil {
+						incoming := nextFlow.getIncoming()
+						if incoming == nil {
+							incoming = make([]*flow, 0)
+						}
+						newIn := append(incoming, &flows[i])
+						m := make([]*flow, 1)
+						m[0] = &nextFlow
+						//设置当前连线出口
+						value.TargetFlowElement = &nextFlow
+						//设置写一个节点入口
+						nextFlow.setIncoming(newIn)
+					}
+					flows[i] = &value
 				}
 			}
 		}
